ce8b1854cbf497bce0ace9a6608b0e9d74839c99
Carlghayes <carlghayes@protonmail.com>
2024-12-07 12:52:10 -0600

    Fixing page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-      hello world
+      <main>
+      Hello world!
       </main>
   );
 }
